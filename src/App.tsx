a0d9395762d185e759ca58aaf6d3198f0a23fa3f
Shane <shane.crotty1@mycit.ie>
2022-03-22 15:51:13 +0000

    setup: MUI npm installs and deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
+      <Button></Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
