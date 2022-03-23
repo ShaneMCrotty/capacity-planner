08e86e71bb3c1add769ba1ba34190f37a1551890
Shane <shane.crotty1@mycit.ie>
2022-03-23 12:18:13 +0000

    wip: div styling, classNames and package structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from '@mui/material/Button';
+import logo from "./assets/logo.svg";
+import "./assets/App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
+    // This is the parent element, which there must be one of
     <div className="App">
-      <Button></Button>
-      <header className="App-header">
+      <header className="AppHeader"></header>
+      <div className="LeftColumn"></div>
+      <div className="RightColumn">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -20,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
