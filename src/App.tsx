c61e3b7c3d53e09669a23c58d75d5592a807f133
Shane <shane.crotty1@mycit.ie>
2022-04-04 23:32:20 +0100

    wip: responsive layout, grid spacing and label props

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,70 @@ import React from "react";
 import logo from "./assets/logo.svg";
 import "./assets/App.css";
 import Button from "@mui/material/Button";
+import { Grid, InputAdornment, InputLabel, TextField } from "@mui/material";
 
 function App() {
   return (
-    // This is the parent element, which there must be one of
+    // Parent element
     <div className="App">
       <header className="AppHeader"></header>
-      <div className="LeftColumn"></div>
-      <div className="RightColumn">
+
+      {/* Our input fields and button functionality */}
+      <div className="Column">
+        <Grid container direction={"column"} spacing={5}>
+          <Grid item>
+            <TextField
+              id={"teamSize"}
+              label="Team Size: "
+              helperText="Enter the number of people on the team here"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">&gt;&gt;</InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id={"daysThisSprint"}
+              label={"Days this Sprint: "}
+              helperText="Enter the number of days for the sprint here"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">&gt;&gt;</InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id={"daysThisSprint"}
+              label={"Working Hours per Day: "}
+              helperText="Enter the number of product. hours each day"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">&gt;&gt;</InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <Button variant="contained">Upload</Button>
+            <Button variant="contained">Download</Button>
+          </Grid>
+        </Grid>
+      </div>
+      {/* Our text and graph representation of the results*/}
+      <div className="Column">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>!Capacity Graph Placeholder!</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Github-link"
+          href="https://github.com/ShaneMCrotty"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          https://github.com/ShaneMCrotty
         </a>
       </div>
     </div>
